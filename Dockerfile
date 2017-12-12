8e18c193903bd87e18c1f94ff67c80a6af6a16e7
DrSnowbird <DrSnowbird@openkbs.org>
2017-12-12 12:30:13 -0500

    ... auto merge scripts in save to remote repo ... Tue Dec 12 12:30:12 EST 2017

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openkbs/jre-mvn-py3-x11
 
 MAINTAINER DrSnowbird "DrSnowbird@openkbs.org"
-
+RUN echo $HOME
 CMD ["/usr/bin/firefox"]
---
+++ run-new.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -x
+
+###################################################
+#### **** Container package information ****
+###################################################
+MY_IP=`ip route get 1|awk '{print$NF;exit;}'`
+DOCKER_IMAGE_REPO="$(basename `pwd`)"
+imageTag=${1:-"openkbs/${DOCKER_IMAGE_REPO}"}
+#PACKAGE=`echo ${imageTag##*/}|tr "/\-: " "_"`
+PACKAGE="${imageTag##*/}"
+baseDataFolder="$HOME/data-docker"
+
+###################################################
+#### ---- Volumes to be mapped (change this!) -----
+#################################################### (examples - some local vars)
+# (exampples)
+# IDEA_PRODUCT_NAME="IdeaIC2017"
+# IDEA_PRODUCT_VERSION="3"
+# IDEA_INSTALL_DIR="${IDEA_PRODUCT_NAME}.${IDEA_PRODUCT_VERSION}"
+# IDEA_CONFIG_DIR=".${IDEA_PRODUCT_NAME}.${IDEA_PRODUCT_VERSION}"
+# IDEA_PROJECT_DIR="IdeaProjects"
+# VOLUMES="${IDEA_CONFIG_DIR} ${IDEA_PROJECT_DIR}"
+
+# ---------------------------
+# MANDATORY Variable: VOLUMES
+# ---------------------------
+VOLUMES="data workspace"
+
+#########################################################################################################
+######################## DON'T CHANGE LINES STARTING BELOW (unless you need to) #########################
+#########################################################################################################
+LOCAL_VOLUME_DIR="${baseDataFolder}/${PACKAGE}"
+DOCKER_VOLUME_DIR="/home/developer"
+
+###################################################
+#### ---- Function: Generate volume mappings  ----
+####      (Don't change!)
+###################################################
+VOLUME_MAP=""
+#### Input: VOLUMES - list of volumes to be mapped
+function generateVolumeMapping() {
+    for vol in $VOLUMES; do
+        echo "$vol"
+        if [[ $vol == "/"* ]]; then
+            # -- non-default /home/developer path; then use the full absolute path --
+            VOLUME_MAP="${VOLUME_MAP} -v ${LOCAL_VOLUME_DIR}/$vol:$vol"
+        else
+            # -- default sub-directory (without prefix absolute path) --
+            VOLUME_MAP="${VOLUME_MAP} -v ${LOCAL_VOLUME_DIR}/$vol:${DOCKER_VOLUME_DIR}/$vol"
+        fi
+        mkdir -p ${LOCAL_VOLUME_DIR}/$vol
+        ls -al ${LOCAL_VOLUME_DIR}/$vol
+    done
+}
+#### ---- Generate Volumes Mapping ----
+generateVolumeMapping
+echo ${VOLUME_MAP}
+
+###################################################
+#### ---- Mostly, you don't need change below ----
+###################################################
+
+#instanceName=my-${1:-${imageTag%/*}}_$RANDOM
+#instanceName=my-${1:-${imageTag##*/}}
+## -- transform '-' and space to '_' 
+instanceName=`echo $(basename ${imageTag})|tr "/\-: " "_"`
+
+echo "---------------------------------------------"
+echo "---- Starting a Container for ${imageTag}"
+echo "---------------------------------------------"
+DISPLAY=${MY_IP}:0 \
+docker run -ti --rm \
+    --name=${instanceName} \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    ${VOLUME_MAP} \
+    ${imageTag}
+
+
