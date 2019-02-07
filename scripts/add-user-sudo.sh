08d6eceba514a6447764ea44030b9b5a115f2b61
Dr.Snowbird <DrSnowbird@openkbs.org>
2019-02-07 00:33:15 -0500

    update add-user-sudo.sh

--- scripts/add-user-sudo.sh
+++ scripts/add-user-sudo.sh
@@ -1,15 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 
 ##################################
 ## ---- user: developer ----
 ##################################
 
-USER=${1:-developer}
-HOME=/home/${USER}
+if [ $# -lt 1 ]; then
+    echo "**** Usage: $(basename $0 <user_name> [,user_password>]) "
+    echo "**** ERROR: Need at lease <user_name> as argument. !"
+    echo "Abort!"
+    echo
+    exit 1;
+fi
 
-sudo groupadd ${USER} && sudo useradd ${USER} -m -d ${HOME} -s /bin/bash -g ${USER} 
-#echo "${USER} ALL=NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+USER_NAME=${1:-developer}
+USER_PASSWORD=${2}
 
-sudo usermod -aG sudo ${USER}
+HOME=/home/${USER_NAME}
 
+if [ "$USER_PASSWORD" != "" ]; then
+    sudo groupadd ${USER_NAME}
+    sudo useradd ${USER_NAME} -m -d ${HOME} -s /bin/bash -g ${USER_NAME} -p $(echo ${USER_PASSWORD} | openssl passwd -1 -stdin) 
+else
+    sudo groupadd ${USER_NAME} && sudo useradd ${USER_NAME} -m -d ${HOME} -s /bin/bash -g ${USER_NAME}
+fi
+
+exit 1
+
+#echo "${USER_NAME} ALL=NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+sudo usermod -aG sudo ${USER_NAME}
 
